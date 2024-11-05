d906626d3dc5f92ee49d2fa90dec2ec32c94488e
bagasadikurniawan <21500022.bagas@sinus.ac.id>
2024-11-05 23:35:34 +0700

    menambahkan dashboard

--- bisma/src/pages/Dashboard.jsx
+++ bisma/src/pages/Dashboard.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import NavBar from '../components/NavBar';
+import DashboardComponent from '../components/DashboardComponent';
 
 const Dashboard = () => {
   return (
         <NavBar>
-
+          <DashboardComponent/>
         </NavBar>
     
   )
