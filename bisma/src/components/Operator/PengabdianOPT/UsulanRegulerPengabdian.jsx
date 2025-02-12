fec909b7b429fbf77a38d3131271f1bcd5d6d244
bagasadikurniawan <21500022.bagas@sinus.ac.id>
2025-02-12 11:20:24 +0700

    perbaiki

--- bisma/src/components/Operator/PengabdianOPT/UsulanRegulerPengabdian.jsx
+++ bisma/src/components/Operator/PengabdianOPT/UsulanRegulerPengabdian.jsx
@@ -25,10 +25,11 @@ const UsulanRegulerPengabdian = () => {
     { label: "Option 3", value: "3" },
   ];
 
+  const user = JSON.parse(localStorage.getItem("user"));
   const metrics = [
     {
       title: "Usulan Draft",
-      count: "Rapek",
+      count: 1,
       icon: <FaFileAlt size={24} />,
       path: "/monitoring/pengabdian/usulan-draft",
     },
--- bisma/src/components/Operator/UsulanReguler.jsx
+++ bisma/src/components/Operator/UsulanReguler.jsx
@@ -25,10 +25,11 @@ const UsulanReguler = () => {
     { label: "Option 3", value: "3" },
   ];
 
+  const user = JSON.parse(localStorage.getItem("user"));
   const metrics = [
     {
       title: "Usulan Draft",
-      count: "Rapek",
+      count: 1,
       icon: <FaFileAlt size={24} />,
       path: "/monitoring-usulan-reguler-usulan-draft",
     },
